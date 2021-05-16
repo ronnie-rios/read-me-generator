00f9948ec260ddecb25e3be80763066ecb29cc9b
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-16 12:54:04 -0500

    added questions array

--- index.js
+++ index.js
@@ -2,7 +2,41 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+ 
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'Title',
+    },
+    {
+        type: 'input',
+        message: 'Please give a description of your project',
+        name: 'Description',
+    },
+    {
+        type: 'input',
+        message: 'What are the neccessary dependencies that must be installed to run this app?',
+        name: 'Installation',
+    },
+    {
+        type: 'list',
+        message: "What license are you using?",
+        name: 'License',
+        choices: ['MIT', 'GNU', 'Apache License 2.0'],
+    },
+    {
+        type:"input",
+        message: "What is your Github username?",
+        name: "Username",   
+    },
+    {
+        type: "input",
+        messaage: 'What is your email address?',
+        name: "Email",
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
