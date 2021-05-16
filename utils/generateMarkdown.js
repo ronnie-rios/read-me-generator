05d395b1423538779a8b00ae6c0a462f96e48b6d
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-16 13:59:42 -0500

    added table of contetns to generate markdown js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,16 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  https://github.com/${data.Username}/${data.title}
+  # Description
+  ${data.description}
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#License)
+  * [Contributors](#contributors)
+  * [Tests](#tests)
+  * [Questions](#questions)
 `;
 }
 
