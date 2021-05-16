53bbdaaa88683e13b37d2990ee2cf8829619b598
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-16 15:24:32 -0500

    added if statements for license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  let icon ="";
+  if (data.License == "MIT") {
+    icon = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  } else if (data.License == 'Apache License 2.0') {
+    icon = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  } else if (data.License == 'GNU') {
+    icon = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  } else {
+    icon = "None"
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// TODO: Create a function that returns the License link
+// If there is no License, return an empty string
+function renderLicenseLink(License) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// TODO: Create a function that returns the License section of README
+// If there is no License, return an empty string
+function renderLicenseSection(License) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.Title}
+  return `# ${data.Title} ${icon}
   https://github.com/${data.Username}/${data.Title}
   # Description
   ${data.Description}
